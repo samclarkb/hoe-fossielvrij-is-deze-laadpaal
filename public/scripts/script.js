b8522fc5541b9552ce281165ce1c6bda9f64e0be
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-05-30 18:11:37 +0200

    geo location broke

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,24 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtY2xhcmtiIiwiYSI6ImNsM3NsMTIwNzAwdWUzYnBjenFwZG1wbjkifQ.fFO4fbulANPhBtywbyszzA';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
+mapboxgl.accessToken =
+	'pk.eyJ1Ijoic2FtY2xhcmtiIiwiYSI6ImNsM3NsMTIwNzAwdWUzYnBjenFwZG1wbjkifQ.fFO4fbulANPhBtywbyszzA'
+
+const succesLocation = position => {
+	console.log(position)
+	setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+const errorLocation = () => {
+	// setupMap([-2.24, 53.48])
+}
+
+navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
+	enableHighAccuracy: true,
+})
+
+const setupMap = center => {
+	const map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: center,
+		zoomn: 10,
+	})
+}
