32b9fb2f6b61f68a89e77e146f1ee1327e047e05
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-05-31 11:11:20 +0200

    green caravan styling

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -17,7 +17,7 @@ navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
 const setupMap = center => {
 	const map = new mapboxgl.Map({
 		container: 'map',
-		style: 'mapbox://styles/mapbox/streets-v11',
+		style: 'mapbox://styles/samclarkb/cl3tx4jtp003x14ny7qk1yfs3',
 		center: center,
 		zoomn: 16,
 	})
