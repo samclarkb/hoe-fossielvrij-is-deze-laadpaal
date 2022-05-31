debff83847a8e0c44631b20575bfa79c6cda510c
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-05-31 21:22:28 +0200

    added image

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,19 +18,33 @@ const setupMap = center => {
 		container: 'map',
 		style: 'mapbox://styles/samclarkb/cl3tx4jtp003x14ny7qk1yfs3',
 		center: center,
-		zoom: 12,
+		zoom: 14,
+		attributionControl: false,
 	})
 
 	// Add geolocate control to the map.
 	map.addControl(
 		new mapboxgl.GeolocateControl({
-			positionOptions: {
+			positionoptions: {
 				enableHighAccuracy: true,
 			},
 			// When active the map will receive updates to the device's location as it changes.
 			trackUserLocation: true,
 			// Draw an arrow next to the location dot to indicate which direction the device is heading.
 			showUserHeading: true,
-		})
+		}),
+		'bottom-right'
 	)
 }
+
+const close = document.getElementById('close')
+const open = document.getElementById('open')
+const popup = document.getElementById('popup-container')
+
+open.addEventListener('click', () => {
+	popup.classList.add('show')
+})
+
+close.addEventListener('click', () => {
+	popup.classList.remove('show')
+})
