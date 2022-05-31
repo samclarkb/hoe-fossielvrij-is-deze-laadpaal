a9a9041382425358d5baa87ca188b260a13090d9
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-05-31 12:05:07 +0200

    add images

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,12 +2,11 @@ mapboxgl.accessToken =
 	'pk.eyJ1Ijoic2FtY2xhcmtiIiwiYSI6ImNsM3NsMTIwNzAwdWUzYnBjenFwZG1wbjkifQ.fFO4fbulANPhBtywbyszzA'
 
 const succesLocation = position => {
-	console.log(position)
 	setupMap([position.coords.longitude, position.coords.latitude])
 }
 
 const errorLocation = () => {
-	// setupMap([-2.24, 53.48])
+	setupMap([-2.24, 53.48])
 }
 
 navigator.geolocation.getCurrentPosition(succesLocation, errorLocation, {
@@ -19,6 +18,19 @@ const setupMap = center => {
 		container: 'map',
 		style: 'mapbox://styles/samclarkb/cl3tx4jtp003x14ny7qk1yfs3',
 		center: center,
-		zoomn: 16,
+		zoom: 12,
 	})
+
+	// Add geolocate control to the map.
+	map.addControl(
+		new mapboxgl.GeolocateControl({
+			positionOptions: {
+				enableHighAccuracy: true,
+			},
+			// When active the map will receive updates to the device's location as it changes.
+			trackUserLocation: true,
+			// Draw an arrow next to the location dot to indicate which direction the device is heading.
+			showUserHeading: true,
+		})
+	)
 }
